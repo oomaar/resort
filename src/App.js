046d6550914a7620efeb9cf4fdaebc59a03b2e1b
Omar <omaar_5@hotmail.com>
2021-03-21 11:41:05 +0200

    complete navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route, Switch } from "react-router-dom";
 import './App.css';
+import { Navbar } from "./Components";
 import { Home, Error, Rooms, SingleRoom } from "./pages";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms/" component={Rooms} />
-      <Route exact path="/rooms/:slug" component={SingleRoom} />
-      <Route component={Error} />
-    </Switch>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
