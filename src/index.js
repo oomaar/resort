aed208526048353c65f1ac5cf076cb2556a9ebcd
Omar <omaar_5@hotmail.com>
2021-03-21 17:55:47 +0200

    setup context API

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { render } from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
+import { RoomProvider } from "./context";
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById('root')
 );
 
