35864987c9bbe7bcda9ef8b4c7589c9112e8f268
Omar <omaar_5@hotmail.com>
2021-03-22 16:57:44 +0200

    complete rooms list

--- src/Components/RoomList.js
+++ src/Components/RoomList.js
@@ -1,8 +1,22 @@
-const RoomList = () => {
+import { Room } from ".";
+
+const RoomList = ({ rooms }) => {
+    if (rooms.length === 0) {
+        return (
+            <div className="empty-search">
+                <h3>unfortunatley no rooms matched your search parameters</h3>
+            </div>
+        )
+    };
+
     return (
-        <div>
-            List
-        </div>
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {rooms.map(item => (
+                    <Room key={item.id} room={item} />
+                ))}
+            </div>
+        </section>
     );
 };
 
